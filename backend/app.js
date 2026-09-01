fa17708318988266e99eef7920da8fdbcdac85d2
Shubham Vishwakarma <shubhamvish0077@gmail.com>
2026-09-01 16:16:14 +0530

    added Handle unhandled Promise Rejection

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 import dotenv from 'dotenv';
 import { connectDatabase } from "./config/dbConnect.js";
+import errorMiddleware from "./middlewares/errors.js";
 
 dotenv.config({ path: "backend/config/config.env" });
 // connect database
@@ -9,12 +10,26 @@ connectDatabase();
 
 app.use(express.json());
 
-// importing routes
-import productRoutes from "./routes/products.js";
 
+// importing all routes
+import productRoutes from "./routes/products.js";
 
 app.use("/api/v1", productRoutes);
 
-app.listen(process.env.PORT, () => {
+//using error Middleware
+app.use(errorMiddleware);
+
+
+const server = app.listen(process.env.PORT, () => {
     console.log(`server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
+});
+
+
+//Handle unhandled Promise Rejection
+process.on('unhandledRejection', (err) => {
+    console.log(`ERROR: ${err}`);
+    console.log('Shutting down server due to unhandled promise rejection');
+    server.close(() => {
+        process.exit(1);
+    });
 });
